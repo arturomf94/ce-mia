4d79aacf2ff692902444d46d678a4fa80160ed0f
arturomf94 <arturomf94@gmail.com>
2019-02-28 18:51:51 -0600

    Create population

--- tarea1.py
+++ tarea1.py
@@ -3,13 +3,25 @@
 import numpy as np
 
 # Parameters:
-P = 10000 # Population size
+P = 5 # Population size
 N = 8 # Number of queens
+
+# Auxiliary functions:
+
+def evaluate(individual):
+     for i in individual:
+         for j in individual[i]:
 
 
 # Create population
 # Random 0-1 matrices of size 8x8
 
 array_seed = np.array([1] * N + [0] * (N * (N - 1)))
+population = []
+for i in range(P):
+    np.random.shuffle(array_seed)
+    population.append(array_seed.reshape((N, N)))
+
+
 
 import pdb;pdb.set_trace()
