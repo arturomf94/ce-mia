c8beec22176dbe9bce648862811fa101904d4512
arturomf94 <arturomf94@gmail.com>
2019-02-28 18:00:15 -0600

    change script name

---
+++ tarea1.py
@@ -0,0 +1,15 @@
+## Evolutionary Algorithm to Solve an N-Queens Problem
+
+import numpy as np
+
+# Parameters:
+P = 10000 # Population size
+N = 8 # Number of queens
+
+
+# Create population
+# Random 0-1 matrices of size 8x8
+
+array_seed = np.array([1] * N + [0] * (N * (N - 1)))
+
+import pdb;pdb.set_trace()
