2af1998a42ee9538ac02a7863c88dda61ff355e9
arturomf94 <arturomf94@gmail.com>
2019-03-01 20:41:55 -0600

    Evaluate individuals

--- tarea1.py
+++ tarea1.py
@@ -3,14 +3,26 @@
 import numpy as np
 
 # Parameters:
-P = 5 # Population size
-N = 8 # Number of queens
+P = 1 # Population size
+N = 4 # Number of queens
 
 # Auxiliary functions:
 
 def evaluate(individual):
-     for i in individual:
-         for j in individual[i]:
+    conflicts = 0
+    for i in range(N):
+        for j in range(N):
+            if individual[i][j] == 1:
+                for p in range(N):
+                    for q in range(N):
+                        if (i != p or j != q) \
+                            and ((i + q == p + j) \
+                                or (i + j == p + q) \
+                                or (i == p) or (j == q)):
+                            if individual[p][q] == 1:
+                                conflicts += 1
+    return conflicts // 2
+
 
 
 # Create population
@@ -18,10 +30,12 @@ def evaluate(individual):
 
 array_seed = np.array([1] * N + [0] * (N * (N - 1)))
 population = []
+conflicts = []
 for i in range(P):
     np.random.shuffle(array_seed)
-    population.append(array_seed.reshape((N, N)))
-
+    individual = array_seed.reshape((N, N))
+    population.append(individual)
+    conflicts.append(evaluate(individual))
 
 
 import pdb;pdb.set_trace()
